049ff99a2249d30433a407b131c18c61f337a23b
Josh Williams <halfs13@gmail.com>
2013-06-11 09:55:01 -0400

    Testing corenlp tree

--- src/com/tadbitstrange/tripletExtractionFromSentence/ExtractionService.java
+++ src/com/tadbitstrange/tripletExtractionFromSentence/ExtractionService.java
@@ -15,9 +15,25 @@
 
 package com.tadbitstrange.tripletExtractionFromSentence;
 
-import java.util.HashMap;
+import org.apache.log4j.Logger;
+
+import edu.stanford.nlp.trees.Tree;
 
 public class ExtractionService {
+	
+	private static Logger log = Logger.getLogger(ExtractionService.class);
+	CoreNlpParser parser;
+	
+	public ExtractionService() {
+		parser = new CoreNlpParser();
+	}
+	
+	public void checkParserFeedback() {
+		Tree tree = parser.parse("My dog has fleas").get(0);
+		log.info(tree.label());
+		log.info(tree.children()[0].label());
+		log.info(tree.children()[0].children()[0].label());
+	}
 	
 	public void tripletExtraction(String s) {
 		extractSubject();
--- src/com/tadbitstrange/tripletExtractionFromSentence/TestDriver.java
+++ src/com/tadbitstrange/tripletExtractionFromSentence/TestDriver.java
@@ -36,24 +36,34 @@ public class TestDriver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//testParser();
+		testExtraction();
+	}
+	
+	private static void testExtraction() {
+		ExtractionService extractor = new ExtractionService();
+		extractor.checkParserFeedback();
+	}
+	
+	private void testParser() {
 		log.debug("Running test driver!");
-		
+
 		Properties props = new Properties();
-	    props.put("annotators", "tokenize, ssplit, parse");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-	    
-	    String text = "Robert Joseph 'Bob' Dole is an American politician who represented Kansas in the United States Senate from 1969 to 1996 and the House of Representatives from 1961 to 1969. In the 1976 presidential election, he was the Republican Party nominee for Vice President and incumbent President Gerald Ford's running mate. In the presidential election of 1996, he was the Republican nominee for President against incumbent Bill Clinton.";
-	    Annotation document = new Annotation(text);
-	    log.info(new Date());
-	    pipeline.annotate(document);
-	    log.info(new Date());
-	    
-	     List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-	    
-	    for(CoreMap sentence: sentences) {
-	      Tree tree = sentence.get(TreeAnnotation.class);
-	      log.info("Tree: " + tree.toString());
-	    }
+		props.put("annotators", "tokenize, ssplit, parse");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		String text = "Robert Joseph 'Bob' Dole is an American politician who represented Kansas in the United States Senate from 1969 to 1996 and the House of Representatives from 1961 to 1969. In the 1976 presidential election, he was the Republican Party nominee for Vice President and incumbent President Gerald Ford's running mate. In the presidential election of 1996, he was the Republican nominee for President against incumbent Bill Clinton.";
+		Annotation document = new Annotation(text);
+		log.info(new Date());
+		pipeline.annotate(document);
+		log.info(new Date());
+
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+		for(CoreMap sentence: sentences) {
+			Tree tree = sentence.get(TreeAnnotation.class);
+			log.info("Tree: " + tree.toString());
+		}
 	}
 
 }
